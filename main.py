9f1c43a4eab3b114f7b522db0c84ef83f796cf09
dragomihail <163417981+dragomihail@users.noreply.github.com>
2024-03-15 18:50:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-def find_max(sequinсe):
-    current_max = sequince[0]
-    for element in sequince:
+def find_max(sequenсe):
+    current_max = sequenсe[0]
+    for element in sequenсe:
         if element > current_max:
             current_max = element
     return current_max
 
 
-def find_min(sequince):
-    current_min=sequince[0]
-    for element in sequince:
+def find_min(sequenсe):
+    current_min=sequenсe[0]
+    for element in sequenсe:
         if element < current_min:
             current_min = element
     return current_min
 
 
-sequince=[4, 10, 2, 5, -2, 0, 1, -4, 7, 3]
+sequenсe=[4, 10, 2, 5, -2, 0, 1, -4, 7, 3]
 
-maximum = find_max(sequince)
-minimum = find_min(sequince)
+maximum = find_max(sequenсe)
+minimum = find_min(sequenсe)
 
 print (maximum)
 print (minimum)
