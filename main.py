6fceb213ca8cb6d611955c594e64e5840e487967
dragomihail <163417981+dragomihail@users.noreply.github.com>
2024-03-15 18:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-def find_max(sequenсe):
-    current_max = sequenсe[0]
-    for element in sequenсe:
+def find_max(sequence):
+    current_max = sequence[0]
+    for element in sequence:
         if element > current_max:
             current_max = element
     return current_max
 
 
-def find_min(sequenсe):
-    current_min=sequenсe[0]
-    for element in sequenсe:
+def find_min(sequence):
+    current_min=sequence[0]
+    for element in sequence:
         if element < current_min:
             current_min = element
     return current_min
 
 
-sequenсe=[4, 10, 2, 5, -2, 0, 1, -4, 7, 3]
+sequence=[4, 10, 2, 5, -2, 0, 1, -4, 7, 3]
 
-maximum = find_max(sequenсe)
-minimum = find_min(sequenсe)
+maximum = find_max(sequence)
+minimum = find_min(sequence)
 
 print (maximum)
 print (minimum)
